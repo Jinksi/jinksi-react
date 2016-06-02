aed15b2d36621d2f7f28b102bd0210ed4689f0f2
Eric Jinks <ericjinks@gmail.com>
2016-06-02 13:24:54 +1000

    Added sketch 002

--- src/components/sketch.js
+++ src/components/sketch.js
@@ -1,21 +1,37 @@
 import React, { Component } from 'react'
 
 import One from '../sketches/001'
+import Two from '../sketches/002'
 
 class Sketch extends Component {
 
   constructor(props){
     super(props)
+    this.state = {
+      currentSketch: 1
+    }
   }
 
   shouldComponentUpdate(){
     return false;
   }
 
+  selectSketch (){
+
+    switch(this.state.currentSketch){
+      case(1):
+        return <One />
+        break
+      case (2):
+        return <Two />
+        break
+    }
+  }
+
   render(){
     return (
       <div id='sketchContainer'>
-        <One />
+        { this.selectSketch() }
       </div>
     )
   }
