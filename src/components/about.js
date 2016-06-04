dbc845102a305fa5452258757cc1b1c646b8769b
Jinksi <ericjinks@gmail.com>
2016-06-04 13:58:40 +1000

    Adjust About text

--- src/components/about.js
+++ src/components/about.js
@@ -3,9 +3,7 @@ import React from 'react';
 export default (props) => {
   return (
     <div className="animated fadeIn">
-      <p>Hello. I am an audio & visual artist from the Gold Coast, Australia.
-      I work as a front-end web developer as part of the <a href="http://thriveweb.com.au">Thrive</a> team.</p>
-      <p>If you want to get in touch, you can contact me at <a href="mailto:ericjinks@gmail.com">ericjinks@gmail.com</a>.</p>
+      <p>Hello. I am an audio & visual artist from the Gold Coast, Australia.</p>
     </div>
   )
 }
