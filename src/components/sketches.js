2c17e1f261b8c8c482f3fb754106e239a9a04db6
Jinksi <ericjinks@gmail.com>
2016-06-05 19:27:13 +1000

    Add sketch006

--- src/components/sketches.js
+++ src/components/sketches.js
@@ -6,16 +6,16 @@ import s001 from '../../sketches/001'
 import s002 from '../../sketches/002'
 import s003 from '../../sketches/003'
 import s005 from '../../sketches/005'
+import s006 from '../../sketches/006'
 
 export default (props) => {
 
   const renderLinks = () => {
     return sketchData.map((sketch, i) => {
-      const seq = padStart(i + 1, 3,'0')
       return (
         <li key={sketch.id}>
-          <Link to={`/sketch/${seq}`}>
-            {seq + ' ' + sketch.title}
+          <Link to={`/sketch/${sketch.id}`}>
+            {sketch.id + ' ' + sketch.title}
           </Link>
         </li>
       )
@@ -50,6 +50,11 @@ export const sketchData = [
     id:'005',
     title: 'perlin Splatter',
     file: s005
+  },
+  {
+    id:'006',
+    title: 'frcs',
+    file: s006
   },
 
 ]
