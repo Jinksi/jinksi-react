434362cb9c384acf9d8264ce9d961958ce2a36d5
Jinksi <ericjinks@gmail.com>
2016-09-01 07:46:28 +1000

    Update sketch016" "

--- sketches/016.js
+++ sketches/016.js
@@ -1,16 +1,19 @@
 export default (p) => {
 
-  let blur = 200
+  let blur = 100
   let gems = []
   let wind
-  let windStrength = 2
+  let windStrength = 1
   let windDynamics = 0.5
   let maxGems = 800
+  let gemHop = 10
+  let gemTime = 30
+  let gemSlow = 0
 
   const Wind = function(c){
     // initialise Perlin offset
     this.xoff = 0
-    this.c = c
+    this.c = windStrength
     this.direction = p.createVector(0,0)
 
     this.calculateForce = function(part){
@@ -22,6 +25,7 @@ export default (p) => {
 
     this.update = function(){
       // create new vector from Perlin Noise
+      this.c = windStrength
       this.direction = p.createVector(
         p.map(p.noise(this.xoff), 0, 1, -1, 1),
         p.map(p.noise(this.xoff + 7), 0, 1, -1, 1)
@@ -34,6 +38,7 @@ export default (p) => {
   }
 
   const Gem = function(x, y, radius){
+    this.col = p.random(1) > .5 ? '#49D582' : '#FFFFFF'
     this.pos = p.createVector(x, y)
     this.vel = p.createVector(0, 0)
     this.acc = p.createVector(0, 0)
@@ -60,22 +65,23 @@ export default (p) => {
       if(this.pos.y > p.height) this.pos.y = 0
 
       p.noStroke()
-      p.fill(255)
+      p.fill(this.col)
       p.ellipse(this.pos.x, this.pos.y, this.radius)
     }
 
     this.spawn = () => {
-
-      const newb = {
-        x: p.random(this.pos.x - 5, this.pos.x + 5),
-        y: p.random(this.pos.y + 10, this.pos.y + 20),
-        radius: this.radius
+      if(p.millis() % gemTime <= 10){
+        const newb = {
+          x: p.random(this.pos.x - 10, this.pos.x + 10),
+          y: p.random(this.pos.y + gemHop, this.pos.y + (gemHop*2)),
+          radius: this.radius
+        }
+        if(newb.x > p.width || newb.y > p.height || this.spawned){
+          return false
+        }
+        gems.push(new Gem(newb.x, newb.y, newb.radius))
+        this.spawned = true
       }
-      if(newb.x > p.width || newb.y > p.height || this.spawned){
-        return false
-      }
-      gems.push(new Gem(newb.x, newb.y, newb.radius))
-      this.spawned = true
     }
   }
 
@@ -84,7 +90,7 @@ export default (p) => {
     p.pixelDensity(2)
     p.createCanvas(p.windowWidth, p.windowHeight)
     p.background(21)
-    wind = new Wind(windStrength)
+    wind = new Wind()
     for(let i = 0; i < p.width; i += 50){
       gems.push(new Gem(i, p.random(0, 20)))
     }
@@ -99,15 +105,33 @@ export default (p) => {
     }
 
     gems.map((gem) => {
+
       gem.spawn()
       gem.applyForce(wind.calculateForce(gem))
+
+        // slow down
+        var force = gem.vel.copy()
+        force.mult(-1)
+        force.setMag(gemSlow)
+        gem.applyForce(force)
+
       gem.display()
     })
     wind.update()
 
+    if(p.mouseX > 0 && p.mouseY > 0){
+      windStrength = p.map(p.mouseX, 0, p.windowWidth, 0.3, 1)
+      // gemHop = p.map(p.mouseY, 0, p.windowHeight, 0.5, 10)
+      // gemTime = p.map(p.mouseX, 0, p.windowWidth, 1, 100)
+      gemSlow = p.map(p.mouseY, 0, p.windowHeight, 0, 0.3)
+    }
+
   }
 
   p.mouseClicked = () => {
+
+  }
+  p.mouseMoved = () => {
 
   }
 
